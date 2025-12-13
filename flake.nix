6f97d6cc8b660e7fc4982c6c1bf4f2e2df25d5f1
polygon <polygon@wh2.tu-dresden.de>
2025-12-14 00:40:16 +0100

    Proper Nix build

--- flake.nix
+++ flake.nix
@@ -1,10 +1,17 @@
 {
   inputs = {
-    #nixpkgs.url = "github:/NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:/NixOS/nixpkgs/nixos-25.11";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, rust-overlay }:
+  outputs = { self, nixpkgs, rust-overlay, naersk }:
     let
       system = "x86_64-linux";
       overlays = [ (import rust-overlay) ];
@@ -21,11 +28,53 @@
         cargo-flamegraph
       ];
       build-deps = with pkgs; [ pkg-config python3 ];
-      runtime-deps = with pkgs; [ rtmidi alsa-lib.dev libGL.dev xorg.libX11.dev jack2.dev xorg.libXcursor.dev xorg.libxcb libxcb-wm ];
+      runtime-deps = with pkgs; [
+        rtmidi
+        alsa-lib.dev
+        libGL.dev
+        xorg.libX11.dev
+        jack2.dev
+        xorg.libXcursor.dev
+        xorg.libxcb
+        libxcb-wm
+      ];
+      naersk-lib = naersk.lib.${system}.override {
+        cargo = rust-bin;
+        rustc = rust-bin;
+      };
     in {
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = dev-deps ++ build-deps ++ runtime-deps;
         LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.alsa-lib ];
+      };
+
+      packages.${system}.mucap = naersk-lib.buildPackage {
+        pname = "mucap";
+        root = ./.;
+        buildInputs = runtime-deps;
+        nativeBuildInputs = build-deps;
+        overrideMain = attrs: {
+          postBuild = ''
+            cargo xtask bundle mucap --release
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp target/bundled/mucap $out/bin/mucap
+            mkdir -p $out/lib/vst3
+            mkdir -p $out/lib/clap
+            cp target/bundled/mucap.clap $out/lib/clap
+            cp -r target/bundled/mucap.vst3 $out/lib/vst3
+          '';
+          #fixupPhase = ''
+          #  wrapProgram $out/bin/${program_name} \
+          #    --prefix LD_LIBRARY_PATH : ${
+          #      pkgs.lib.makeLibraryPath runtime-deps
+          #    }
+          #'';
+          #patchPhase = ''
+          #   sed -i s/\"dynamic\"// Cargo.toml
+          #'';
+        };
       };
 
     };
